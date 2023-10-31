5a098a1d182162ed37f2bf56325629b34f2f2d5e
sheshnath <124980051+sheshnath-at-knoldus@users.noreply.github.com>
2023-10-31 17:07:19 +0530

    Update webpack.config.js

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -14,6 +14,6 @@ module.exports = {
   devServer: {
     host: '0.0.0.0',
     port: 8080,
-    // Additional devServer configuration options can be added if needed.
+    // Additional devServer configuration options can be added here if needed.
   },
 };
